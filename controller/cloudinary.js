c3617dd2865b75161aa1a69862652c075f2b0ecb
timilehin <joeltimmy7@gmail.com>
2022-03-21 15:36:19 +0100

    created the cloudinary controller file and method

---
+++ controller/cloudinary.js
@@ -0,0 +1,38 @@
+const cloudinary = require("cloudinary").v2;
+
+exports.uploadFile = (file) => {
+    return new Promise((resolve, reject) => {
+        cloudinary.config({
+            cloud_name: process.env.CLOUD_NAME,
+            api_key: process.env.API_KEY,
+            api_secret: process.env.API_SECRET,
+            secure: true,
+        });
+
+        cloudinary.uploader.upload(
+            file,
+            {
+                resourceType: fileType,
+
+                public_id: fileName,
+
+                overwrite: true,
+            },
+            (error, result) => {
+                if (error) {
+                    fs.unlinkSync(file);
+                    resolve({
+                        status: false,
+                        error: error.message,
+                    });
+                } else {
+                    fs.unlinkSync(file);
+                    resolve({
+                        status: true,
+                        fileUrl: result.secure_url,
+                    });
+                }
+            }
+        );
+    });
+};
